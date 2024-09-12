ee56e6e52ff40a40073de96b0545716da3ea384f
Daniel Oliveira <shinayser@gmail.com>
2024-09-12 16:27:50 -0300

    Impproving Personalidade promp

--- powershell/init-ai.ps1
+++ powershell/init-ai.ps1
@@ -1,6 +1,6 @@
 #Todos os comandos listados aqui necessitam do módulo https://github.com/mkht/PSOpenAI
 function Personalidade-AI {
-    $comando = "Dê um nome e escreva um parágrafo contando sobre o seguinte npc, dado este json de sua personalidade: $(Personalidade | ConvertTo-Json)"
-    $resposta = Request-ChatCompletion -Message $comando
+    $comando = "Dê um nome e escreva um parágrafo, separado por bullets com 3 tópicos: 'Nome', 'Aparência' e 'Trejeitos', contando sobre o seguinte npc, dado este json de sua personalidade: $(Personalidade | ConvertTo-Json)"
+    $resposta = Request-ChatCompletion -Message $comando -Model 'gpt-4o'
     $resposta.Answer
 }
